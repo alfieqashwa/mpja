7d468c57929c352cb83fdf60be2999f9e59b3519
Alfie Qashwa <alfieqashwa@gmail.com>
2026-01-24 22:18:27 +0700

    fix: finished section about-us

--- app/(home)/about-us.tsx
+++ app/(home)/about-us.tsx
@@ -1,5 +1,5 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { ChevronRight, PlayCircle } from "lucide-react";
+import { FileIcon, PackageIcon, SecureIcon } from "@/components/icons/about";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export function AboutUs() {
@@ -20,7 +20,7 @@ export function AboutUs() {
 
       <div className="mt-12 items-center grid grid-cols-1 gap-8 md:grid-cols-2 md:items-start">
         <section className="space-y-6 h-full">
-          <article className="px-6 py-9 space-y-6 flex flex-col items-start border rounded-2xl relative border-slate-200 shadow-xs bg-white overflow-hidden bg-[linear-gradient(to_right,rgba(244,63,94,0.15)_1px,transparent_1px),linear-gradient(to_bottom,rgba(244,63,94,0.15)_1px,transparent_1px)] bg-size-[45px_45px]">
+          <article className="px-6 py-9 space-y-6 flex flex-col items-start border rounded-2xl relative border-slate-200 shadow-sm bg-white overflow-hidden bg-[linear-gradient(to_right,rgba(244,63,94,0.15)_1px,transparent_1px),linear-gradient(to_bottom,rgba(244,63,94,0.15)_1px,transparent_1px)] bg-size-[45px_45px]">
             <p className="text-3xl font-semibold text-primary">
               MPJA Learning is dedicated to empowering individuals through
               education, community, and accessible learning resources.
@@ -33,7 +33,7 @@ export function AboutUs() {
               </span>
             </p>
           </article>
-          <div className="relative rounded-2xl border border-slate-200 shadow-xs bg-white overflow-hidden bg-[linear-gradient(to_right,rgba(244,63,94,0.15)_1px,transparent_1px),linear-gradient(to_bottom,rgba(244,63,94,0.15)_1px,transparent_1px)] bg-size-[45px_45px]">
+          <div className="relative rounded-2xl border border-slate-200 shadow-sm bg-white overflow-hidden bg-[linear-gradient(to_right,rgba(244,63,94,0.15)_1px,transparent_1px),linear-gradient(to_bottom,rgba(244,63,94,0.15)_1px,transparent_1px)] bg-size-[45px_45px]">
             <div className="grid grid-cols-3 py-12">
               {[
                 { title: "Professional Course", count: "6+" },
@@ -60,6 +60,52 @@ export function AboutUs() {
             className=""
           />
         </div>
+      </div>
+
+      <div className="mt-8 grid grid-cols-1 gap-6 md:grid-cols-3">
+        {[
+          {
+            title: "Expert-Led Instruction",
+            description:
+              "Learn from experienced professionals and educators with real-world insights.",
+            icon: <SecureIcon />,
+          },
+          {
+            title: "Flexible Online Learning",
+            description:
+              "Access your courses anytime and progress at your own pace.",
+            icon: <PackageIcon />,
+          },
+          {
+            title: "Quality-Assured Content",
+            description:
+              "Every course is designed to be clear, structured, and purpose-driven.",
+            icon: <FileIcon />,
+          },
+        ].map((item, idx) => (
+          <div
+            className="rounded-2xl border border-slate-200 bg-white p-6 shadow-sm"
+            key={`${idx}-${item.title}`}
+          >
+            {/* icon */}
+            <div
+              className={cn(
+                "flex items-center justify-center rounded-full bg-[#FFF1F2] text-rose-500",
+                idx === 0 ? "size-16" : "size-14.5",
+              )}
+            >
+              {item.icon}
+            </div>
+
+            {/* content */}
+            <div className="flex flex-col space-y-3 mt-10">
+              <h3 className="text-2xl font-bold text-primary">{item.title}</h3>
+              <p className="text-base leading-relaxed text-secondary font-medium">
+                {item.description}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
