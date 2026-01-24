93861b2d66f57c9202cc564576b45af4519a647b
Alfie Qashwa <alfieqashwa@gmail.com>
2026-01-24 16:53:49 +0700

    feat: finished design buttons

--- app/designs/buttons-design.tsx
+++ app/designs/buttons-design.tsx
@@ -1,15 +1,170 @@
+import { ArrowLeftIcon, ArrowRightIcon } from "@/components/icons/arrows";
 import { Button } from "@/components/ui/button";
 
 export function ButtonsDesign() {
   return (
-    <section>
+    <div>
       <h1 className="text-2xl">Buttons Design</h1>
-      <div className="mt-8 flex gap-8">
-        <Button variant={"default"}>Button</Button>
-        <Button variant={"secondary"}>Button</Button>
-        <Button variant={"outline"}>Button</Button>
-        <Button variant={"ghost"}>Button</Button>
+      <div className="grid gap-8 columns-4 mt-8">
+        <section className="space-x-8">
+          <h2>Disabled XL</h2>
+          <Button disabled variant={"default"} size={"xl"}>
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button disabled variant={"secondary"} size={"xl"}>
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button disabled variant={"outline"} size={"xl"}>
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button disabled variant={"ghost"} size={"xl"}>
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+        </section>
+        <section className="space-x-8">
+          <h2>XL</h2>
+          <Button
+            variant={"default"}
+            size={"xl"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"secondary"}
+            size={"xl"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"outline"}
+            size={"xl"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"ghost"}
+            size={"xl"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+        </section>
+        <section className="space-x-8">
+          <h2>LG</h2>
+          <Button
+            variant={"default"}
+            size={"lg"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"secondary"}
+            size={"lg"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"outline"}
+            size={"lg"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"ghost"}
+            size={"lg"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+        </section>
+        <section className="space-x-8">
+          <h2>MD / size=default</h2>
+          <Button
+            variant={"default"}
+            size={"default"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button variant={"secondary"} className="hover:cursor-pointer">
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button variant={"outline"} className="hover:cursor-pointer">
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button variant={"ghost"} className="hover:cursor-pointer">
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+        </section>
+        <section className="space-x-8">
+          <h2>SM</h2>
+          <Button
+            variant={"default"}
+            size={"sm"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"secondary"}
+            size={"sm"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"ghost"}
+            size={"sm"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+        </section>
+        <section className="space-x-8">
+          <h2>XS</h2>
+          <Button
+            variant={"default"}
+            size={"xs"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"secondary"}
+            size={"xs"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"outline"}
+            size={"xs"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+          <Button
+            variant={"ghost"}
+            size={"xs"}
+            className="hover:cursor-pointer"
+          >
+            <ArrowLeftIcon /> Button <ArrowRightIcon />
+          </Button>
+        </section>
       </div>
-    </section>
+    </div>
   );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,28 +1,30 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-bold transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-bold transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none outline-none focus-visible:shadow-[0px_0px_0px_4px_rgba(251,113,133,0.25)] active:scale-[0.98] active:shadow-inner",
   {
     variants: {
       variant: {
+        // default:
+        //   "bg-gradient-to-b from-rose-500 to-rose-600 text-white shadow-[0_0_0_1px_rgba(251,113,133,1),0_1px_3px_-1px_rgba(251,113,133,1),inset_0_1px_0_0_rgba(251,113,133,1)] hover:opacity-90",
         default:
-          "bg-gradient-to-b from-rose-500 to-rose-600 text-white shadow-[0_0_0_1px_rgba(251,113,133,1),0_1px_3px_-1px_rgba(251,113,133,1),inset_0_1px_0_0_rgba(251,113,133,1)] hover:opacity-90",
-        destructive:
-          "bg-destructive text-white hover:bg-destructive/90",
-        outline:
-          "border border-slate-200 bg-white shadow-xs hover:bg-slate-50 text-slate-700",
+          "text-white bg-[linear-gradient(180deg,rgba(255,255,255,0.16)_0%,rgba(255,255,255,0.16)_100%),linear-gradient(180deg,#F43F5E_0%,#E11D48_100%)] shadow-[0px_1px_3px_-1px_#FB7185,0px_0px_0px_1px_#FB7185,inset_0px_1px_0px_#FB7185] hover:bg-[linear-gradient(180deg,rgba(255,255,255,0.16)_0%,rgba(255,255,255,0.16)_100%),linear-gradient(180deg,#FECDD3_0%,#E11D48_100%)]",
         secondary:
-          "bg-pink-50 text-rose-500 hover:bg-pink-100",
+          "bg-[#FDF2F8] text-rose-600 shadow-[0px_1px_3px_-1px_#FB7185,0px_0px_0px_1px_#FB7185,inset_0px_1px_0px_#FB7185]",
+        outline:
+          "bg-transparent text-rose-600 border border-[#E11D48] shadow-none",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground",
+          "bg-transparent text-rose-600 p-0 h-auto shadow-none hover:bg-[#FDF2F8]",
         link: "text-rose-500 underline-offset-4 hover:underline",
+        destructive: "bg-destructive text-white hover:bg-destructive/90",
       },
       size: {
         default: "h-11 px-5 py-2.5",
+        xs: "h-6 w-24 gap-1.5 px-2.5 py-1 rounded-xl",
         sm: "h-9 rounded-lg gap-1.5 px-4",
         lg: "h-12 rounded-xl px-6 text-base",
         xl: "h-14 rounded-xl px-8 text-base",
@@ -35,8 +37,8 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
-)
+  },
+);
 
 function Button({
   className,
@@ -46,9 +48,9 @@ function Button({
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button"
+  const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
@@ -58,7 +60,7 @@ function Button({
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
-  )
+  );
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
