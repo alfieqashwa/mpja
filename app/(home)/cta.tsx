ea5120f393fb55c951081b8a995c58853cfac917
Alfie Qashwa <alfieqashwa@gmail.com>
2026-01-26 17:04:10 +0700

    fix: setup cta section

--- app/(home)/cta.tsx
+++ app/(home)/cta.tsx
@@ -1,22 +1,35 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export function Cta() {
   return (
-    <section className="kurt bg-linear-to-b from-rose-500 to-pink-100">
-      <div className="">
-        <h2 className="text-3xl font-bold text-slate-900 sm:text-4xl mb-4">
-          Ready to Start Your Learning Journey?
+    <section className="kurt bg-linear-to-b from-rose-50 from-10% via-rose-100 via-40% to-85% to-rose-50/50 relative">
+      <div className="pl-19 pt-26 w-1/2 space-y-6 pb-30">
+        <h2 className="text-6xl font-bold text-primary">
+          Ready to Start Your <br />
+          <span className="text-danger">Learning Journey?</span>
         </h2>
-        <p className="max-w-xl mx-auto text-slate-600 mb-8">
-          Join thousands of students who have transformed their careers with our
-          courses.
+        <p className="text-secondary text-2xl font-semibold">
+          Join learners advancing their careers through <br /> structured,
+          self-paced online courses.
         </p>
-        <div className="flex flex-wrap justify-center gap-4">
-          <Button size="xl">Get Started Today</Button>
+        <div className="flex flex-wrap justify-start gap-3">
+          <Button size="xl">Explore courses</Button>
           <Button variant="secondary" size="xl">
-            Browse Courses
+            Sign up free
           </Button>
         </div>
+      </div>
+
+      {/* <div className="relative w-full aspect-4/3 transform rotate-y-[-5deg] rotate-x-[5deg] -top-60"> */}
+      <div className="bottom-0 right-0 absolute">
+        <Image
+          src="/images/cta-image.png"
+          alt="Dashboard preview"
+          width={720}
+          height={800}
+          priority
+        />
       </div>
     </section>
   );
