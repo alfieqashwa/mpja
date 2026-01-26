95653f8487f8d16b9d0025d55aaf70dc9e89d893
Alfie Qashwa <alfieqashwa@gmail.com>
2026-01-26 20:39:53 +0700

    style: fixing hero section

--- app/(home)/Hero.tsx
+++ app/(home)/Hero.tsx
@@ -7,29 +7,30 @@ import Image from "next/image";
 
 export function Hero() {
   return (
-    <section
-      id="home"
-      className="relative overflow-hidden pt-24 from-[#FFF1F2] to-pink-50 rounded-3xl bg-linear-to-b kurt"
-    >
+    <section id="home" className="relative overflow-hidden pt-24 rounded-3xl">
+      {/* Background Image */}
+      <div className="absolute inset-0 bg-[url('/images/hero-bg-image.svg')] bg-cover bg-center bg-no-repeat">
+        {/* fade to white */}
+        <div className="absolute bottom-0 h-48 w-full bg-linear-to-t from-white to-transparent" />
+      </div>
+
       <div className="relative">
         <div className="flex flex-col items-center mt-20">
-          <h1 className="text-4xl font-bold tracking-tight text-slate-900 sm:text-6xl">
-            Empower Your{" "}
-            <span className="space-x-2">
-              <span className="inline-flex size-10 items-center justify-center rounded-lg bg-rose-600 shadow-lg shadow-rose-200">
-                <BooksIcon className="size-6 text-white" />
-              </span>
-              <span className="text-rose-600">Professional Skills</span>
+          <h1 className="text-4xl font-bold tracking-tight text-primary sm:text-6xl">
+            Empower Your
+            <span className="inline-flex size-15 mx-3 items-center justify-center rounded-2xl bg-rose-600 shadow-lg shadow-rose-200">
+              <BooksIcon className="size-8 text-white" />
             </span>
+            <span className="text-[#E11D48]">Professional Skills</span>
           </h1>
           <h1 className="text-4xl font-bold tracking-tight text-slate-900 sm:text-5xl lg:leading-[1.1]">
             at your own pace
-            <span className="ml-2 inline-flex size-10 items-center justify-center rounded-lg bg-rose-600 shadow-lg shadow-rose-200">
-              <BookOpenIcon className="size-6 text-white" />
+            <span className="ml-3 inline-flex size-15 items-center justify-center rounded-2xl bg-rose-600 shadow-lg shadow-rose-200">
+              <BookOpenIcon className="size-8 text-white fill-white" />
             </span>
           </h1>
 
-          <p className="py-6 text-lg text-slate-500 text-center leading-relaxed font-semibold">
+          <p className="py-6 text-2xl text-secondary text-center leading-relaxed font-semibold">
             A modern learning platform for students and professionals to build
             practical knowledge at their
             <br />
@@ -52,36 +53,17 @@ export function Hero() {
           </div>
         </div>
 
-        {/* Starts Dashboard Preview Image */}
-        <div className="hidden lg:block perspective-1000 kurt">
-          <div className="relative w-full aspect-4/3 transform rotate-y-[-5deg] rotate-x-[5deg] -top-60">
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Dashboard Preview"
-              fill
-              priority
-              className="object-contain drop-shadow-3xl mask-fade-half"
-              // className="object-contain z-10"
-            />
-            {/* <Image
-              src="/images/white-cloud-left.png"
-              alt="White Cloud Left"
-              width={420}
-              height={280}
-              priority
-              className="pointer-events-none absolute top-1/2 -translate-y-1/2 left-0 z-20"
-            />
-            <Image
-              src="/images/white-cloud-right.png"
-              alt="White Cloud Right"
-              width={420}
-              height={280}
-              priority
-              className="pointer-events-none absolute top-1/2 -translate-y-1/2 -right-30 z-20"
-            /> */}
-          </div>
+        {/* Dashboard Preview Image */}
+        <div className="kurt mt-20">
+          <Image
+            src="/images/dashboard-preview.png"
+            alt="Dashboard Preview"
+            width={1200}
+            height={856}
+            priority
+            className="h-auto w-11/12 drop-shadow-3xl"
+          />
         </div>
-        {/* Ends Dashboard Preview Image */}
       </div>
     </section>
   );
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -12,7 +12,7 @@ import { Testimonials } from "./testimonials";
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-white font-sans relative">
+    <div className="bg-white relative">
       <Navbar />
 
       <main className="space-y-24">
