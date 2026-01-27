6e69842372ab827dde6af9777b260c6dc58566c8
Alfie Qashwa <alfieqashwa@gmail.com>
2026-01-27 17:08:34 +0700

    fix: setup login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,11 @@
-"use client"
-
+import { CopyrightMpja } from "@/components/copyright"
 import Image from "next/image"
+import { LoginForm } from "./login-form"
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen grid lg:grid-cols-2 kurt">
-      <div className="relative thom">
+    <div className="min-h-screen grid lg:grid-cols-2">
+      <div className="relative">
         <div className="absolute top-12 left-1/2 -translate-x-1/2">
           <Image
             src="/img/logo.png"
@@ -17,23 +17,23 @@ export default function LoginPage() {
         </div>
 
         {/* Login form */}
-        <div className="mx-auto w-full max-w-md kurt mt-68">
+        <div className="mx-auto w-full max-w-md mt-68">
           <article className="text-center space-y-3">
             <h1 className="text-5xl font-bold text-primary">Welcome back</h1>
             <p className="text-2xl font-semibold text-secondary">
               Sign in to continue your learning journey.
             </p>
           </article>
-          <form className="pt-8">
-            {/*
-              - Email & Password
-              - Remember me & Forgot password link 
-              - Sign in Button
-              - New to MPJA? Create an account link
-            */}
-          </form>
+          <LoginForm />
+          <p className="pt-8 text-lg space-x-2 text-center">
+            <span className="text-secondary font-semibold">New to MPJA?</span>
+            <span className="text-[#E11D48] font-bold">Create an account</span>
+          </p>
           <footer>{/* make reusable footer */}</footer>
         </div>
+        <footer className="absolute w-full bottom-0 px-10">
+          <CopyrightMpja />
+        </footer>
       </div>
 
       <div className="relative hidden lg:block h-full w-full">
@@ -45,7 +45,6 @@ export default function LoginPage() {
           priority
         />
       </div>
-      {/* Overlay */}
     </div>
   )
 }
