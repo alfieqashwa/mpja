f5bbbedc55ebd119e522cb0057b95b4fcb165e4a
Alfie Qashwa <alfieqashwa@gmail.com>
2026-01-31 03:23:19 +0700

    feat: created nested course page

---
+++ app/(auth)/authed/course/detail-completed/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardHeader } from "../../_components/dashboard-header"
+
+export default function DetailCompletedPage() {
+  return (
+    <div>
+      <DashboardHeader title="Course" />
+      <h2>Detail Completed</h2>
+    </div>
+  )
+}
---
+++ app/(auth)/authed/course/detail-continue/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardHeader } from "../../_components/dashboard-header"
+
+export default function DetailContinuePage() {
+  return (
+    <div>
+      <DashboardHeader title="Course" />
+      <h2>Detail Continue</h2>
+    </div>
+  )
+}
