2f43dadedd2adb4541b3e509a8dc74e80b909adf
Alfie Qashwa <alfieqashwa@gmail.com>
2026-01-31 03:22:51 +0700

    feat: configure list of courses

---
+++ app/(auth)/authed/course/course-card.tsx
@@ -0,0 +1,192 @@
+"use client"
+
+import { BookOpenIcon } from "@/components/custom-icons"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import { cn } from "@/lib/utils"
+import {
+  BookOpen,
+  ChartNoAxesColumnIncreasing,
+  Heart,
+  Medal,
+  ScrollText,
+} from "lucide-react"
+import Link from "next/link"
+import { useState } from "react"
+
+type CategoryCourse = "not-started" | "on-going" | "completed" | "wishlist"
+
+export type Course = {
+  courseId: number
+  category: CategoryCourse
+  title: string
+  completedLessons: number
+  totalLessons: number
+  currentProgress: number
+  maxProgress: number
+  completedAssignments: number
+  totalAssignments: number
+  buttonTitle: string
+  href: string
+  dueDate?: string
+  price?: number
+  isLiked?: boolean
+}
+
+export function CourseCard({
+  courseId,
+  category,
+  title,
+  completedLessons,
+  totalLessons,
+  currentProgress,
+  maxProgress,
+  completedAssignments,
+  totalAssignments,
+  buttonTitle,
+  href,
+  dueDate,
+  price,
+  isLiked,
+}: Course) {
+  const [likedCard, setLikedCard] = useState(isLiked)
+
+  // const isLiked = likedCards[courseId]
+
+  const toggleHeart = (id: number, isLiked: boolean) => {
+    console.log(id)
+    console.log(isLiked)
+  }
+
+  return (
+    <div id={courseId.toString()} className="bg-white p-4 rounded-2xl w-full">
+      {/* Image Frame */}
+      <div
+        className="relative flex aspect-video items-center justify-center rounded-xl bg-[#FFF1F2]"
+        style={{
+          backgroundImage: `
+                    radial-gradient(circle, rgba(255, 255, 255, 0.8) 1.5px, transparent 1.5px),
+                    radial-gradient(circle, rgba(255, 255, 255, 0.35) 1px, transparent 1px),
+                    radial-gradient(circle, rgba(255, 255, 255, 0.2) 1px, transparent 1px)
+  `,
+          backgroundSize: "14px 14px, 18px 18px, 24px 24px",
+          backgroundPosition: "0 0, 11px 16px, 7px 9px",
+        }}
+      >
+        <BookOpenIcon className="text-danger size-28" />
+      </div>
+      {/* CTA */}
+      <div className={cn("pt-3", "flex mt-5 justify-between items-start")}>
+        <article className="text-left space-y-2">
+          <h3 className="text-2xl capitalize font-bold text-primary whitespace-nowrap">
+            {title}
+          </h3>
+          {category === "wishlist" && (
+            <p className="text-secondary text-lg font-medium">
+              177 Student | Certification
+            </p>
+          )}
+        </article>
+        {category === "wishlist" && (
+          <Heart
+            size={32}
+            onClick={() => setLikedCard((toggle) => !toggle)}
+            className={cn(
+              "cursor-pointer transition-all duration-200 hover:scale-110",
+              likedCard
+                ? "text-rose-600 fill-rose-600"
+                : "text-rose-600 bg-transparent",
+            )}
+          />
+        )}
+      </div>
+      <div className="my-3 bg-[#F8FAFC] rounded-sm px-2 py-1 flex justify-between">
+        <article className="space-y-1 py-2.5 px-5 flex-1 justify-center items-center">
+          <h4 className="text-xs text-secondary text-center">Lesson</h4>
+          <section className="flex items-center gap-1 justify-center">
+            <BookOpen size={16} className="text-secondary" />
+            <p className="text-primary text-sm font-bold  whitespace-nowrap">
+              <span>{completedLessons}</span>/<span>{totalLessons}</span>
+            </p>
+          </section>
+        </article>
+        <div>
+          <Separator orientation="vertical" />
+        </div>
+        <article className="space-y-1 py-2.5 px-5 flex-1 justify-center items-center">
+          <h4 className="text-xs text-secondary text-center">Progress</h4>
+          <section className="flex items-center justify-center gap-x-1">
+            <ChartNoAxesColumnIncreasing size={16} className="text-secondary" />
+            <p className="text-primary text-sm font-bold whitespace-nowrap">
+              <span>{currentProgress}</span>/<span>{maxProgress}</span>
+            </p>
+          </section>
+        </article>
+        <div>
+          <Separator orientation="vertical" />
+        </div>
+        <article className="space-y-1 py-2.5 px-5 flex-1 justify-center items-center">
+          <h4 className="text-xs text-secondary text-center">Assignment</h4>
+          <section className="flex items-center gap-1 justify-center">
+            <ScrollText size={16} className="text-secondary" />
+            <p className="text-primary text-sm font-bold whitespace-nowrap">
+              <span>{completedAssignments}</span>/
+              <span>{totalAssignments}</span>
+            </p>
+          </section>
+        </article>
+      </div>
+
+      <div className="my-3 h-0.5 w-full bg-[linear-gradient(to_right,#e2e8f0_50%,transparent_50%)] bg-size-[24px_2px] bg-repeat-x"></div>
+
+      {/* Footer + Button */}
+      <div className="flex justify-between items-center">
+        <CourseCategoryInfo
+          category={category}
+          dueDate={dueDate as string}
+          price={price as number}
+        />
+        <Link href={`/authed/course/${encodeURIComponent(href)}`}>
+          <Button size={"default"} className="cursor-pointer">
+            {buttonTitle}
+          </Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+const CourseCategoryInfo = ({
+  category,
+  dueDate,
+  price,
+}: {
+  category: CategoryCourse
+  dueDate: string
+  price: number
+}) => {
+  if (category === "wishlist")
+    return (
+      <p className="text-2xl text-danger font-bold">
+        {new Intl.NumberFormat("ja-JP", {
+          style: "currency",
+          currency: "JPY",
+        }).format(price as number)}
+      </p>
+    )
+  if (category === "on-going")
+    return <p className="text-secondary text-xs">Due Date {dueDate}</p>
+
+  if (category === "completed")
+    return (
+      <div className="space-y-1">
+        <p className="text-sm text-[#D97706] font-bold">
+          <Medal size={24} className="inline" />{" "}
+          <span>Completed Certificated</span>
+        </p>
+        <p className="text-secondary text-xs">Due Date {dueDate}</p>
+      </div>
+    )
+
+  return <p className="text-secondary text-xs">Not Started</p>
+}
--- app/(auth)/authed/course/page.tsx
+++ app/(auth)/authed/course/page.tsx
@@ -1,9 +1,186 @@
+import { Button } from "@/components/ui/button"
 import { DashboardHeader } from "../_components/dashboard-header"
+import { DashboardTitle } from "../_components/dashboard-title"
+import { DashboardWrapper } from "../_components/dashboard-wrapper"
+import { Course, CourseCard } from "./course-card"
+
+/*
+ * === Button Title ===
+ * Not Started: Start Learning,
+ * On Going: Continue
+ * Completed: View
+ * Whishlist: Join Course Now
+ */
+
+const COURSES = [
+  {
+    courseId: 1,
+    category: "not-started",
+    title: "halal indonesian",
+    completedLessons: 0,
+    totalLessons: 13,
+    currentProgress: 0,
+    maxProgress: 100,
+    completedAssignments: 0,
+    totalAssignments: 13,
+    buttonTitle: "Start Learning",
+    href: "detail-continue",
+  },
+  {
+    courseId: 2,
+    category: "on-going",
+    title: "halal indonesian",
+    completedLessons: 10,
+    totalLessons: 13,
+    currentProgress: 90,
+    maxProgress: 100,
+    completedAssignments: 10,
+    totalAssignments: 13,
+    buttonTitle: "Continue",
+    href: "detail-continue",
+    dueDate: "24 Dec 2026",
+  },
+  {
+    courseId: 3,
+    category: "completed",
+    title: "halal indonesian",
+    completedLessons: 13,
+    totalLessons: 13,
+    currentProgress: 100,
+    maxProgress: 100,
+    completedAssignments: 13,
+    totalAssignments: 13,
+    buttonTitle: "View",
+    href: "detail-completed",
+    dueDate: "24 Dec 2026",
+  },
+  {
+    courseId: 4,
+    category: "wishlist",
+    title: "halal indonesian",
+    completedLessons: 0,
+    totalLessons: 13,
+    currentProgress: 0,
+    maxProgress: 100,
+    completedAssignments: 0,
+    totalAssignments: 13,
+    buttonTitle: "Join Course Now",
+    href: "detail-completed",
+    price: 7_000,
+    isLiked: true,
+  },
+  {
+    courseId: 5,
+    category: "completed",
+    title: "halal indonesian",
+    completedLessons: 13,
+    totalLessons: 13,
+    currentProgress: 100,
+    maxProgress: 100,
+    completedAssignments: 13,
+    totalAssignments: 13,
+    buttonTitle: "View",
+    href: "detail-completed",
+    dueDate: "24 Dec 2026",
+  },
+  {
+    courseId: 6,
+    category: "on-going",
+    title: "halal indonesian",
+    completedLessons: 10,
+    totalLessons: 13,
+    currentProgress: 90,
+    maxProgress: 100,
+    completedAssignments: 10,
+    totalAssignments: 13,
+    buttonTitle: "Continue",
+    href: "detail-continue",
+    dueDate: "24 Dec 2026",
+  },
+  {
+    courseId: 7,
+    category: "wishlist",
+    title: "halal indonesian",
+    completedLessons: 0,
+    totalLessons: 13,
+    currentProgress: 0,
+    maxProgress: 100,
+    completedAssignments: 0,
+    totalAssignments: 13,
+    buttonTitle: "Join Course Now",
+    href: "detail-completed",
+    price: 7_000,
+    isLiked: true,
+  },
+  {
+    courseId: 8,
+    category: "not-started",
+    title: "halal indonesian",
+    completedLessons: 0,
+    totalLessons: 13,
+    currentProgress: 0,
+    maxProgress: 100,
+    completedAssignments: 0,
+    totalAssignments: 13,
+    buttonTitle: "Start Learning",
+    href: "detail-continue",
+  },
+] satisfies Course[]
 
 export default function CoursePage() {
   return (
     <div>
       <DashboardHeader title="course" />
+      <DashboardWrapper>
+        <div className="flex justify-between items-center">
+          <DashboardTitle
+            title="Courses you’re enrolled in."
+            subTitle="See your active courses, progress, and what to learn next"
+          />
+          <CourseCategoryButton />
+        </div>
+
+        {/* Course List */}
+        <div className="pt-3.5 grid grid-cols-3 gap-x-4 gap-y-5">
+          {COURSES.map((c, idx) => (
+            <CourseCard
+              courseId={c.courseId}
+              category={c.category}
+              title={c.title}
+              completedLessons={c.completedLessons}
+              totalLessons={c.totalLessons}
+              currentProgress={c.currentProgress}
+              maxProgress={c.maxProgress}
+              completedAssignments={c.completedAssignments}
+              totalAssignments={c.totalAssignments}
+              buttonTitle={c.buttonTitle}
+              href={c.href}
+              dueDate={c.dueDate}
+              price={c.price}
+              isLiked={c.isLiked}
+              key={`${idx}-${c.courseId}`}
+            />
+          ))}
+        </div>
+      </DashboardWrapper>
     </div>
   )
 }
+
+const CourseCategoryButton = () => (
+  <div className="flex items-center gap-2 border bg-white p-2 rounded-full">
+    <Button>All Course</Button>
+    <Button variant={"ghost"} className="text-secondary">
+      Not Started
+    </Button>
+    <Button variant={"ghost"} className="text-secondary">
+      On Going
+    </Button>
+    <Button variant={"ghost"} className="text-secondary">
+      Completed
+    </Button>
+    <Button variant={"ghost"} className="text-secondary">
+      Wishlist
+    </Button>
+  </div>
+)
