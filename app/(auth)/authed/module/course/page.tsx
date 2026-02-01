3d5994bc550617935f748d0e799cfb6beac808c7
Alfie Qashwa <alfieqashwa@gmail.com>
2026-02-01 23:46:50 +0700

    fix(WIP): setup page module course

--- app/(auth)/authed/module/course/page.tsx
+++ app/(auth)/authed/module/course/page.tsx
@@ -1,15 +1,9 @@
 import { DashboardHeader } from "@/app/(auth)/authed/_components/dashboard-header"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+import Image from "next/image"
 import { Suspense } from "react"
 import { DashboardWrapper } from "../../_components/dashboard-wrapper"
 import { BreadcrumbModule } from "./breadcrum-module"
+import { Button } from "@/components/ui/button"
 
 // custom sidebar
 export default function ModulePage() {
@@ -21,6 +15,55 @@ export default function ModulePage() {
           {/* Breadcumb */}
           <BreadcrumbModule />
         </Suspense>
+        <article className="space-y-4">
+          <h1 className="text-primary font-bold text-5xl">
+            Define Halal, Haram, and Mashbooh
+          </h1>
+          <p className="text-secondary font-medium text-lg">
+            1/10 | 45 minutes | Rayhan Kuswara | English
+          </p>
+        </article>
+        <div className="space-y-4">
+          <div className="relative aspect-video">
+            <Image
+              src="/img/Video.png"
+              alt="Video e-learning module"
+              fill
+              priority
+              className="object-cover rounded-4xl"
+            />
+          </div>
+          <p className="text-secondary font-medium">
+            MPJA Learning supports Muslim professionals and halal industry
+            participants through accessible, high-quality education. Our
+            platform offers structured online courses designed for long study
+            sessions, with clear learning pathways, assessments, and
+            certification outcomes. MPJA Learning supports Muslim professionals
+            and halal industry participants through accessible, high-quality
+            education. Our platform offers structured online courses designed
+            for long study sessions, with clear learning pathways, assessments,
+            and certification outcomes.MPJA Learning supports Muslim
+            professionals and halal industry participants through accessible,
+            high-quality education. Our platform offers structured online
+            courses designed for long study sessions, with clear learning
+            pathways, assessments, and certification outcomes.
+          </p>
+        </div>
+        {/* Pagination */}
+        <footer className="flex justify-between items-center">
+          <Button
+            variant={"ghost"}
+            className="cursor-pointer bg-[#E2E8F0] text-secondary hover:bg-slate-300"
+          >
+            Back
+          </Button>
+          <section className="space-x-5">
+            <span className="font-bold text-primary">
+              Next Topic : Halal in Daily Life and Business
+            </span>
+            <Button className="cursor-pointer">Continue</Button>
+          </section>
+        </footer>
       </DashboardWrapper>
     </div>
   )
