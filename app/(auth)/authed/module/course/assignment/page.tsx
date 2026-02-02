cc5844319b1d0665c9839822b9451dc5d8922678
Alfie Qashwa <alfieqashwa@gmail.com>
2026-02-02 20:25:16 +0700

    feat: config confetti

--- app/(auth)/authed/module/course/assignment/page.tsx
+++ app/(auth)/authed/module/course/assignment/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-import { useState } from "react"
-import { DashboardHeader } from "../../../_components/dashboard-header"
-import { DashboardWrapper } from "../../../_components/dashboard-wrapper"
+import { Button } from "@/components/ui/button"
 import {
   Field,
   FieldContent,
@@ -11,8 +9,13 @@ import {
   FieldTitle,
 } from "@/components/ui/field"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Switch } from "@/components/ui/switch"
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import confetti from "canvas-confetti"
+import { useCallback, useEffect, useState } from "react"
+import { DashboardHeader } from "../../../_components/dashboard-header"
+import { DashboardWrapper } from "../../../_components/dashboard-wrapper"
+import { useRouter } from "next/navigation"
 
 type MultipleChoice = {
   id: string
@@ -45,13 +48,47 @@ const MULTIPLE_CHOICE = [
 
 export default function AssignmentPage() {
   const [selected, setSelected] = useState<string | null>(null)
-  // useState<"default" | "correct" | "wrong" | "celebrate">("default")
+  const [isCompleted, setIsCompleted] = useState(false)
 
+  const triggerRealisticConfetti = useCallback(() => {
+    const count = 200
+    const defaults = { origin: { y: 0.7 } }
+
+    function fire(particleRatio: number, opts: confetti.Options) {
+      confetti({
+        ...defaults,
+        ...opts,
+        particleCount: Math.floor(count * particleRatio),
+      })
+    }
+
+    fire(0.25, { spread: 26, startVelocity: 55 })
+    fire(0.2, { spread: 60 })
+    fire(0.35, { spread: 100, decay: 0.91, scalar: 0.8 })
+    fire(0.1, { spread: 120, startVelocity: 25, decay: 0.92, scalar: 1.2 })
+    fire(0.1, { spread: 120, startVelocity: 45 })
+  }, [])
+
+  // Trigger Confetti when Switch is toggled
+  useEffect(() => {
+    if (isCompleted) {
+      triggerRealisticConfetti()
+    }
+  }, [isCompleted, triggerRealisticConfetti])
+
+  const router = useRouter()
   const userChoice = MULTIPLE_CHOICE.find((c) => c.id === selected)
+
   return (
     <div>
       <DashboardHeader title="Course" left="left-100" />
-      <DashboardWrapper>
+      <DashboardWrapper className="relative">
+        <TemporaryCompletedQuiz
+          isCorrect={userChoice?.answer}
+          isCompleted={isCompleted}
+          setIsCompleted={setIsCompleted}
+        />
+
         <div className="rounded-2xl border bg-white shado-sm p-6">
           <h2 className="text-primary font-bold text-2xl">Quiz 1 (25 Score)</h2>
           <section className="pt-8">
@@ -106,17 +143,59 @@ export default function AssignmentPage() {
         </div>
       </DashboardWrapper>
       <footer className="bg-white h-15 fixed z-99 border bottom-0 px-4 flex items-center justify-between w-full max-w-7xl">
-        <p className="text-lg font-bold text-primary">Assigment 1/3</p>
+        <p
+          className={cn(
+            "text-lg font-bold text-primary",
+            isCompleted ? "invisible" : "",
+          )}
+        >
+          Assigment 1/3
+        </p>
         <section className="space-x-5">
-          <Button
-            variant={"ghost"}
-            className="cursor-pointer bg-[#E2E8F0] text-secondary hover:bg-slate-300"
-          >
-            Back
-          </Button>
+          {isCompleted ? (
+            <Button
+              variant={"ghost"}
+              onClick={() => setIsCompleted(false)}
+              className="cursor-pointer bg-[#E2E8F0] text-secondary hover:bg-slate-300"
+            >
+              Retake Quiz
+            </Button>
+          ) : (
+            <Button
+              variant={"ghost"}
+              onClick={() => router.back()}
+              className="cursor-pointer bg-[#E2E8F0] text-secondary hover:bg-slate-300"
+            >
+              Back
+            </Button>
+          )}
           <Button className="cursor-pointer">Continue</Button>
         </section>
       </footer>
     </div>
   )
 }
+
+const TemporaryCompletedQuiz = ({
+  isCorrect,
+  isCompleted,
+  setIsCompleted,
+}: {
+  isCorrect: boolean | undefined
+  isCompleted: boolean
+  setIsCompleted: React.Dispatch<React.SetStateAction<boolean>>
+}) => (
+  <div
+    className={cn(
+      "right-1/8 items-center mt-4",
+      isCorrect ? "fixed" : "hidden",
+    )}
+  >
+    <Switch
+      id="debug-mode"
+      checked={isCompleted}
+      onCheckedChange={setIsCompleted}
+      className="data-[state=checked]:bg-emerald-500 data-[state=unchecked]:bg-secondary cursor-pointer"
+    />
+  </div>
+)
